3359d9857d1b09345cb43ffd344221948a247cad
Sakshee Yande <145008370+Sakshee2104@users.noreply.github.com>
2025-06-11 12:37:59 +0530

    Update page.tsx

--- app/blog/devnastra-expansion/page.tsx
+++ app/blog/devnastra-expansion/page.tsx
@@ -35,7 +35,7 @@ export default function BlogPost() {
             </div>
 
             <Image
-              src="/placeholder.svg?height=400&width=800"
+              src="/global.jpg?height=400&width=800"
               alt="DevAstra Launch"
               width={800}
               height={400}
